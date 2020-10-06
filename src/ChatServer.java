46827624fb9c417ca593629b5ed5fafa7b8730f7
Alevig21 <alevig21@bergen.org>
2020-10-06 00:11:45 -0400

    Added PCHAT, updated CHAT, and added REACT

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -4,6 +4,8 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.io.BufferedReader;
@@ -40,7 +42,7 @@ public class ChatServer {
                         clientList.add(client);
                     }
                     
-                    System.out.println("added client " + name);
+                    System.out.println(name + " has joined");
 
                     //handle client business in another thread
                     pool.execute(new ClientHandler(client));
@@ -61,6 +63,8 @@ public class ChatServer {
             this.client = client;
         }
 
+        HashMap reactionHash = new HashMap<String, String>();
+
         /**
 		 * Broadcasts a message to all clients connected to the server.
 		 */
@@ -80,6 +84,46 @@ public class ChatServer {
             
         }
 
+        public void broadcastChat(String msg, String name) {
+            try {
+                ArrayList<ClientConnectionData> newClientList = clientList;
+                newClientList.remove(client.getUserName());
+                System.out.println("Broadcasting -- " + msg);
+                synchronized (newClientList) {
+                    for (ClientConnectionData c : newClientList){
+                        c.getOut().println(msg);
+                        // c.getOut().flush();
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println("broadcast caught exception: " + ex);
+                ex.printStackTrace();
+            }
+
+        }
+
+        public void broadcastPChat(String msg, String name) {
+            try {
+                ArrayList<ClientConnectionData> newClientList = new ArrayList<>();
+                for (int i = 0; i < clientList.size(); i++) {
+                    if (clientList.get(i).getUserName() == name) {
+                        newClientList.add(clientList.get(i));
+                    }
+                }
+                System.out.println("Broadcasting -- " + msg);
+                synchronized (newClientList) {
+                    for (ClientConnectionData c : newClientList){
+                        c.getOut().println(msg);
+                        // c.getOut().flush();
+                    }
+                }
+            } catch (Exception ex) {
+                System.out.println("broadcast caught exception: " + ex);
+                ex.printStackTrace();
+            }
+
+        }
+
         @Override
         public void run() {
             try {
@@ -90,6 +134,7 @@ public class ChatServer {
                 //notify all that client has joined
                 broadcast(String.format("WELCOME %s", client.getUserName()));
 
+                reactionAdder(reactionHash);
                 
                 String incoming = "";
 
@@ -97,11 +142,28 @@ public class ChatServer {
                     if (incoming.startsWith("CHAT")) {
                         String chat = incoming.substring(4).trim();
                         if (chat.length() > 0) {
-                            String msg = String.format("CHAT %s %s", client.getUserName(), chat);
-                            broadcast(msg);    
+                            String msg = String.format("%s: %s", client.getUserName(), chat);
+                            broadcastChat(msg, client.getUserName());
                         }
                     } else if (incoming.startsWith("QUIT")){
                         break;
+                    } else if (incoming.startsWith("PCHAT")) {
+                        String nameAndChat = incoming.substring(5).trim();
+                        String name = "";
+                        int i = 0;
+                        while (!(nameAndChat.charAt(i) == (' '))) {
+                            name += nameAndChat.charAt(i);
+                        }
+                        String chat = nameAndChat.substring(name.length() + 1);
+                        if (chat.length() > 0) {
+                            String msg = String.format("%s (private): %s", client.getUserName(), chat);
+                            broadcastPChat(msg, client.getUserName());
+                        }
+                    } else if (incoming.startsWith("REACT")) {
+                        String key = incoming.substring(5).trim();
+                        String reaction = (String) reactionHash.get(key);
+                        String msg = String.format("%s Reacted: %s", client.getUserName(), reaction);
+                        broadcastChat(msg, client.getUserName());
                     }
                 }
             } catch (Exception ex) {
@@ -125,7 +187,60 @@ public class ChatServer {
 
             }
         }
-        
+        public void reactionAdder (HashMap<String, String> h) {
+            h.put("smile", ":)");
+            h.put("frown", ":(");
+            h.put("wink-smile", ";)");
+            h.put("wink-frown", ";(");
+            h.put("backwards-smile", "(:");
+            h.put("backwards-frown", "):");
+            h.put("backwards-wink-smile", "(;");
+            h.put("backwards-wink-frown", ");");
+            h.put("shrug", "¯\\_(ツ)_/¯");
+            h.put("lenny", "( ͡° ͜ʖ ͡°)");
+            h.put("flipping-table", "(╯°□°)╯︵ ┻━┻");
+            h.put("table-down", "┬─┬ノ( º _ ºノ)");
+            h.put("alarmed-table-flip", "(┛◉Д◉)┛彡┻━┻");
+            h.put("pointing-at-flipped-table", "(☞ﾟヮﾟ)☞ ┻━┻");
+            h.put("flipping-table-look", "(┛ಠ_ಠ)┛彡┻━┻");
+            h.put("yeah", "( •_•)\n" +
+                    "( •_•)>⌐■-■\n" +
+                    "(⌐■_■)");
+            h.put("bear", "ʕ•ᴥ•ʔ");
+            h.put("cat", "ฅ^•ﻌ•^ฅ");
+            h.put("fighting", "(ง •̀_•́)ง");
+            h.put("monocle", "ಠ_ರೃ");
+            h.put("music", "───────────────⚪────\n" +
+                    "◄◄⠀▐▐ ⠀►►⠀⠀ ⠀ 1:17 / 3:48 ⠀ ───○⠀ ᴴᴰ ⚙ ❐ ⊏⊐");
+            h.put("helicopter", "▬▬▬.◙.▬▬▬\n" +
+                    "═▂▄▄▓▄▄▂\n" +
+                    "◢◤ █▀▀████▄▄▄▄◢◤\n" +
+                    "█▄ █ █▄ ███▀▀▀▀▀▀▀╬\n" +
+                    "◥█████◤\n" +
+                    "══╩══╩═\n" +
+                    "╬═╬\n" +
+                    "╬═╬\n" +
+                    "╬═╬\n" +
+                    "╬═╬ Hello?\n" +
+                    "╬═╬☻/\n" +
+                    "╬═╬/▌\n" +
+                    "╬═╬/ \\");
+            h.put("(/╰^._.^╯\\)", "bat");
+            h.put("lenny-shrug", "¯\\_( ͡° ͜ʖ ͡°)_/¯");
+            h.put("bird", "ˎ₍•ʚ•₎ˏ");
+            h.put("cat-2", "(=^･ｪ･^=)");
+            h.put("angry", "ಠ_ಠ");
+            h.put("walking", "ᕕ( ᐛ )ᕗ");
+            h.put("bunny", "[)_(]\n" +
+                    "(‘ * ’)\n" +
+                    "(_ _)");
+            h.put("creepy", "ಠᴗಠ");
+            h.put("bear-2", "(^◕ᴥ◕^)");
+            h.put("lenny-money", "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]");
+            h.put("sad", "ಥ_ಥ");
+            h.put("flower-on-face", "(◕‿◕✿)");
+            h.put("zombie", "[¬º-°]¬");
+        }
     }
 
 }
